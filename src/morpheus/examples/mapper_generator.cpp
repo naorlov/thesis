717128fb392788ec7ad28fca1ff88c14b61de3d7
Nikita Orlov <nikitao466@gmail.com>
2020-01-04 02:31:32 +0300

    Added source code for example mappers and build scripts

---
+++ src/morpheus/examples/mapper_generator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    for (std::string line; std::getline(std::cin, line);) {
+
+    }
+    return 0;
+}
